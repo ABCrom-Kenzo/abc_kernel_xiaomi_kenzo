7e60cef9cb812b4b40a061f563bf509412d262f5
Linus Walleij <linus.walleij@linaro.org>
2015-10-12 18:52:59 +0300

    ARM64: kasan: print memory assignment
    This prints out the virtual memory assigned to KASan in the
    boot crawl along with other memory assignments, if and only
    if KASan is activated.
    
    Example dmesg from the Juno Development board:
    
    Memory: 1691156K/2080768K available (5465K kernel code, 444K rwdata,
    2160K rodata, 340K init, 217K bss, 373228K reserved, 16384K cma-reserved)
    Virtual kernel memory layout:
        kasan   : 0xffffff8000000000 - 0xffffff9000000000   (    64 GB)
        vmalloc : 0xffffff9000000000 - 0xffffffbdbfff0000   (   182 GB)
        vmemmap : 0xffffffbdc0000000 - 0xffffffbfc0000000   (     8 GB maximum)
                  0xffffffbdc2000000 - 0xffffffbdc3fc0000   (    31 MB actual)
        fixed   : 0xffffffbffabfd000 - 0xffffffbffac00000   (    12 KB)
        PCI I/O : 0xffffffbffae00000 - 0xffffffbffbe00000   (    16 MB)
        modules : 0xffffffbffc000000 - 0xffffffc000000000   (    64 MB)
        memory  : 0xffffffc000000000 - 0xffffffc07f000000   (  2032 MB)
          .init : 0xffffffc0007f5000 - 0xffffffc00084a000   (   340 KB)
          .text : 0xffffffc000080000 - 0xffffffc0007f45b4   (  7634 KB)
          .data : 0xffffffc000850000 - 0xffffffc0008bf200   (   445 KB)
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit ee7f881b59de4e0e0be250fd0c5d4ade3e30ec34)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -289,6 +289,9 @@ void __init mem_init(void)
 #define MLK_ROUNDUP(b, t) b, t, DIV_ROUND_UP(((t) - (b)), SZ_1K)
 
 	pr_notice("Virtual kernel memory layout:\n"
+#ifdef CONFIG_KASAN
+		  "    kasan   : 0x%16lx - 0x%16lx   (%6ld GB)\n"
+#endif
 		  "    vmalloc : 0x%16lx - 0x%16lx   (%6ld MB)\n"
 #ifdef CONFIG_SPARSEMEM_VMEMMAP
 		  "    vmemmap : 0x%16lx - 0x%16lx   (%6ld MB)\n"
@@ -298,6 +301,9 @@ void __init mem_init(void)
 		  "      .init : 0x%p" " - 0x%p" "   (%6ld kB)\n"
 		  "      .text : 0x%p" " - 0x%p" "   (%6ld kB)\n"
 		  "      .data : 0x%p" " - 0x%p" "   (%6ld kB)\n",
+#ifdef CONFIG_KASAN
+		  MLG(KASAN_SHADOW_START, KASAN_SHADOW_END),
+#endif
 		  MLM(VMALLOC_START, VMALLOC_END),
 #ifdef CONFIG_SPARSEMEM_VMEMMAP
 		  MLM((unsigned long)virt_to_page(PAGE_OFFSET),
